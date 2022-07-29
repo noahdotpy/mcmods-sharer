63e55e779b6344130ea37a1bfde9cd90d7e1b85e
Noah <80379684+noahdotpy@users.noreply.github.com>
2022-07-29 12:02:19 +1000

    Formatting code, using `logging` module, added check for mod loader (if it is supported)

--- functions.py
+++ functions.py
@@ -10,11 +10,66 @@ import platform
 
 from sys import exit
 
+import logging
+
+# Logging config
+FMT = "[ {levelname} ]: {message}"
+FORMATS = {
+    logging.DEBUG: FMT,
+    logging.INFO: f"\33[36m{FMT}\33[0m",
+    logging.WARNING: f"\33[33m{FMT}\33[0m",
+    logging.ERROR: f"\33[31m{FMT}\33[0m",
+    logging.CRITICAL: f"\33[1m\33[31m{FMT}\33[0m",
+}
+
+
+GITHUB_REPO = "https://github.com/noahdotpy/mcmods-sharer/"
+
+SUPPORTED_MOD_LOADERS = ["fabric", "quilt", "forge"]
+
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        log_fmt = FORMATS[record.levelno]
+        formatter = logging.Formatter(log_fmt, style="{")
+        return formatter.format(record)
+
+
+handler = logging.StreamHandler()
+handler.setFormatter(CustomFormatter())
+logging.basicConfig(level=logging.DEBUG, handlers=[handler])
+
+log = logging.getLogger()
+
+
+def open_browser(link, unsupported_then_show_link=False):
+    user_os = platform.system()
+
+    if user_os == "Linux":
+        subprocess.run(f"xdg-open {link}", shell=True, check=True)
+    elif user_os == "Windows":
+        subprocess.run(f"start {link}")
+    elif user_os == "Darwin":
+        subprocess.run(f"open {link}", shell=True, check=True)
+    else:
+        log.warning("Opening sites in default browser not supported in this OS.")
+        if unsupported_then_show_link:
+            print(f"Link: {link}")
+
 
 def export_mods(py_args):
+
+    if py_args.mod_loader.lower() not in SUPPORTED_MOD_LOADERS:
+        log.error(
+            f"{py_args.mod_loader.title()} is not supported, if you think this is absolutely ridiculous, please, open a new issue: {GITHUB_REPO}"
+        )
+
     mods = {"pacmc": {}, "manual": {}, "http_dl": {}}
-    files = [f for f in listdir(py_args.archive_path) if isfile(
-        join(py_args.archive_path, f))]
+    files = [
+        f
+        for f in listdir(py_args.archive_path)
+        if isfile(join(py_args.archive_path, f))
+    ]
 
     mods["game_version"] = py_args.game_version
     mods["mod_loader"] = py_args.mod_loader
@@ -22,15 +77,15 @@ def export_mods(py_args):
     # Adding mods to mods dictionary
     for mod_file in files:
         # PacMC
-        if mod_file.endswith('.pacmc.jar'):
+        if mod_file.endswith(".pacmc.jar"):
             # modrinth
-            if '_mr_' in mod_file:
-                splitted = mod_file.split('_mr_')
+            if "_mr_" in mod_file:
+                splitted = mod_file.split("_mr_")
                 mod_repo = "modrinth"
 
             # curseforge
-            if '_cf_' in mod_file:
-                splitted = mod_file.split('_cf_')
+            if "_cf_" in mod_file:
+                splitted = mod_file.split("_cf_")
                 mod_repo = "curseforge"
 
             mod_slug = splitted[0]
@@ -38,98 +93,109 @@ def export_mods(py_args):
                 "repo": mod_repo,
             }
 
-        if mod_file == ('.mcmods.json'):
+        if mod_file == (".mcmods.json"):
             # Manual
-            with open(f"{py_args.archive_path}/{mod_file}", 'r') as f:
+            with open(f"{py_args.archive_path}/{mod_file}", "r") as f:
                 mcmods_mods = json.load(f)
 
             for mod in mcmods_mods["manual"]:
-                mods["manual"][mod] = {
-                    "link": mcmods_mods["manual"][mod]["link"]}
+                mods["manual"][mod] = {"link": mcmods_mods["manual"][mod]["link"]}
 
             # HTTP DL
-            with open(f"{py_args.archive_path}/{mod_file}", 'r') as f:
+            with open(f"{py_args.archive_path}/{mod_file}", "r") as f:
                 mcmods_mods = json.load(f)
 
             for mod in mcmods_mods["http_dl"]:
-                mods["http_dl"][mod] = {
-                    "link": mcmods_mods["http_dl"][mod]["link"]}
+                mods["http_dl"][mod] = {"link": mcmods_mods["http_dl"][mod]["link"]}
 
     # Render the output
     json_dump = json.dumps(mods)
 
     if py_args.file[0]:
-        with open(py_args.file[0], 'w') as out_file:
+        with open(py_args.file[0], "w") as out_file:
             print(json_dump, file=out_file)
-            print("INFO: Created file at: " + py_args.file[0])
+            log.info("Created file at: " + py_args.file[0])
 
     if py_args.copy:
         pyperclip.copy(json_dump)
-        print("INFO: Mod list copied to clipboard!")
+        log.info("Mod list copied to clipboard")
 
 
 def import_mods(py_args):
     """
     Implement auto PacMC downloading (pacmc install sodium) with user choosing archive to download to
-    Implement auto opening manual and HTTP_DL in browser 
+    Implement auto opening manual and HTTP_DL in browser
     cmd_syntax = {
         "linux": "xdg-open https://google.com",
         "windows": "start https://google.com",
         "macos": "open https://google.com"
     }
     """
-    with open(py_args.file_path, 'r') as f:
+    with open(py_args.file_path, "r") as f:
         mods = json.load(f)
 
-    user_os = platform.system()
-    
-    if user_os == 'Linux':
-        open_site_command = "xdg-open"
-    elif user_os == 'Windows':
-        open_site_command = "start"
-    elif user_os == 'Darwin':
-        open_site_command = "open"
-    else:
-        raise Exception(f"{user_os} is not currently supported.")
+    log.info(
+        f"This modlist is meant to be used with {mods['mod_loader']} for {mods['game_version']}"
+    )
 
-    print(f"WARNING: This modlist is meant to be used with {mods['mod_loader']} for {mods['game_version']}!!")
-    
     # PACMC
     # Installing mods with PACMC
     if len(mods["pacmc"]) > 0:
-        pacmc_install_confirm = input(
-            f"\nYou are about to install {len(mods['pacmc'])} mods into the default archive with pacmc...\nAre you sure you want to do this (Y/n)? ").strip().lower()
+        print()
+        log.info(
+            f"You are about to install {len(mods['pacmc'])} mods into the default archive with pacmc..."
+        )
+        pacmc_install_confirm = (
+            input("Are you sure you want to do this (Y/n)? ").strip().lower()
+        )
         if pacmc_install_confirm in ["n", "no"]:
-            print("Not installing mods with pacmc...")
+            log.info("Skipping section: pacmc...")
         else:
-            print("Installing mods with pacmc...")
-            subprocess.run(f"pacmc install {' '.join([mods['pacmc'][mod]['repo']+'/'+mod for mod in mods['pacmc']])}",
-                        shell=True, check=True)
+            log.info("Installing mods with pacmc...")
+            subprocess.run(
+                f"pacmc install {' '.join([mods['pacmc'][mod]['repo']+'/'+mod for mod in mods['pacmc']])}",
+                shell=True,
+                check=True,
+            )
     else:
-        print("\nNo PacMC mods, skipping...")
+        log.info("\nNo PacMC mods, skipping...")
 
     # MANUAL
     if len(mods["manual"]) > 0:
-        manual_install_confirm = input(
-            f"\nYou are about to open {len(mods['manual'])} mod links in the default browser (manually download)...\nAre you sure you want to do this (Y/n)? ").strip().lower()
+        print()
+        log.info(
+            f"You are about to open {len(mods['manual'])} mod links in the default browser (manually download)..."
+        )
+        manual_install_confirm = (
+            input("Are you sure you want to do this (Y/n)? ").strip().lower()
+        )
         if manual_install_confirm in ["n", "no"]:
-            print("Not opening links in browser...")
+            log.info("Skipping section: manual...")
         else:
-            print("Opening links in browser...")
+            log.info("Opening links in browser...")
             for mod in mods["manual"]:
-                subprocess.run(f"{open_site_command} {mods['manual'][mod]['link']}", shell=True, check=True)
+                open_browser(
+                    mods["manual"][mod]["link"], unsupported_then_show_link=True
+                )
     else:
-        print("\nNo manual install mods, skipping...")
-    
+        log.info("\nNo manual install mods, skipping...")
+
     # HTTP_DL
     if len(mods["manual"]) > 0:
-        http_dl_install_confirm = input(
-            f"\nYou are about to download {len(mods['manual'])} mods from an untrusted source...\nAre you sure you trust these links (Y/n)? ").strip().lower()
+        print()
+        log.info(
+            f"You are about to download {len(mods['manual'])} mods from an untrusted source..."
+        )
+        http_dl_install_confirm = (
+            input("Are you sure you trust these links (Y/n)? ").strip().lower()
+        )
         if http_dl_install_confirm in ["n", "no"]:
-            print("Not opening links in browser...")
+            log.info("Skipping section: http_dl...")
         else:
-            print("Opening download links in browser...")
+            log.info("Opening download links in browser...")
             for mod in mods["http_dl"]:
-                subprocess.run(f"{open_site_command} {mods['http_dl'][mod]['link']}", shell=True, check=True)
+                open_browser(
+                    mods["http_dl"][mod]["link"], unsupported_then_show_link=True
+                )
     else:
-        print("\nNo http_dl mods, skipping...")
+        log.info("\nNo http_dl mods, skipping...")
--- mcmod-sharer.py
+++ mcmod-sharer.py
@@ -1,42 +1,85 @@
-def main():
-    from functions import export_mods, import_mods
+from functions import export_mods, import_mods
 
-    import argparse
+import argparse
+
+
+def main():
 
     parser = argparse.ArgumentParser(
         description="Scans your specified folder for mods downloaded with the PacMC package manager and outputs all valid PacMC mods in the folder."
     )
-    subparsers = parser.add_subparsers(help='commands')
+
+    parser.add_argument(
+        "--log",
+        "-l",
+        help="Log output to a file",
+        nargs=1,
+        metavar=("<file_path>"),
+        action="store",
+    )
+
+    subparsers = parser.add_subparsers(help="commands")
 
     # Export command
-    export_parser = subparsers.add_parser('export', help='List contents')
-    
-    export_parser.add_argument('--game-version', '-gv', action='store', default=[''], nargs=1, metavar=(
-        '<game_version>',), help='Minecraft version.', required=True)
-    
-    export_parser.add_argument('--mod-loader', '-ml', action='store', default=[''], nargs="*", metavar=(
-        '<mod_loader>',), help='Minecraft mod loader.', required=True)
-    
-    export_parser.add_argument('archive_path', help='Path to the archive you are pulling mods list from.')
-    
-    export_parser.add_argument('--file', '-f', action='store', default=[''], nargs=1, metavar=(
-        '<file_path>',), help='Write mod list to a file.')
-    
-    export_parser.add_argument('--copy', '-c', action='store_true', default=False,
-                       help='Copy mod list to clipboard.')
-    
+    export_parser = subparsers.add_parser("export", help="List contents")
+
+    export_parser.add_argument(
+        "--game-version",
+        "-gv",
+        action="store",
+        default=[""],
+        nargs=1,
+        metavar=("<game_version>",),
+        help="Minecraft version.",
+        required=True,
+    )
+
+    export_parser.add_argument(
+        "--mod-loader",
+        "-ml",
+        action="store",
+        default=[""],
+        nargs="*",
+        metavar=("<mod_loader>",),
+        help="Minecraft mod loader.",
+        required=True,
+    )
+
+    export_parser.add_argument(
+        "archive_path", help="Path to the archive you are pulling mods list from."
+    )
+
+    export_parser.add_argument(
+        "--file",
+        "-f",
+        action="store",
+        default=[""],
+        nargs=1,
+        metavar=("<file_path>",),
+        help="Write mod list to a file.",
+    )
+
+    export_parser.add_argument(
+        "--copy",
+        "-c",
+        action="store_true",
+        default=False,
+        help="Copy mod list to clipboard.",
+    )
+
     export_parser.set_defaults(func=export_mods)
-    
+
     # Import command
-    import_parser = subparsers.add_parser('import', help='List contents')
-    
-    import_parser.add_argument('file_path', help='File to import from (json).')
-    
+    import_parser = subparsers.add_parser("import", help="List contents")
+
+    import_parser.add_argument("file_path", help="File to import from (json).")
+
     import_parser.set_defaults(func=import_mods)
-    
+
     py_args = parser.parse_args()
-    
+
     py_args.func(py_args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
